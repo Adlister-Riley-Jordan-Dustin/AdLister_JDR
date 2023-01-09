f711340ea1b5d2fb471d73b7b248e63df7fc423f
RileyK646 <RileyKing646@gmail.com>
2023-01-09 17:12:18 -0500

    Update migration.sql

--- migration.sql
+++ migration.sql
@@ -15,6 +15,7 @@ CREATE TABLE ads  (
     user_id INT UNSIGNED NOT NULL,
     title VARCHAR(240) NOT NULL,
     description TEXT NOT NULL,
+    price INT UNSIGNED NOT NULL ,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(id)
         ON DELETE CASCADE
@@ -25,9 +26,9 @@ VALUES
     ('rileyk', 'riley@gmail.com', 'password'),
     ('dustinz', 'dustin@gmail.com', 'password');
 
--- INSERT INTO ads(user_id, id, title, description, price)
--- VALUES
---     (1, 1, 'Giant Panda', 'Not sure why you would need this, but also...why not', 99999.99),
---     (1,2,'Seven Ice Cream Cones', 'For when you are extra hungry and sad', 32.50),
---     (2, 3, 'Decorative Throw Pillows', 'Unfortunately covered in Canadian Maple leafs', 20.99),
---     (3, 4, 'Sega Genesis', 'If you bend the wire the right way it works!', 19.99);
+INSERT INTO ads(user_id, id, title, description, price)
+VALUES
+   (1, 1, 'Giant Panda', 'Not sure why you would need this, but also...why not', 99999.99),
+   (1,2,'Seven Ice Cream Cones', 'For when you are extra hungry and sad', 32.50),
+    (2, 3, 'Decorative Throw Pillows', 'Unfortunately covered in Canadian Maple leafs', 20.99),
+   (3, 4, 'Sega Genesis', 'If you bend the wire the right way it works!', 19.99);
