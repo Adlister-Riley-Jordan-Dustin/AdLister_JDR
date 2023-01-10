3ffeccb2c08b2a895f22be136ab409d31c372522
Dustin Zook <dustin.w.zook@gmail.com>
2023-01-10 14:15:13 -0600

    completed edit user info

--- src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
+++ src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
@@ -18,11 +18,24 @@ public class ViewProfileServlet extends HttpServlet {
             return;
         }
         User user = (User) request.getSession().getAttribute("user");
-        Long userId = (Long) request.getSession().getAttribute("userId");
         request.setAttribute("ads", DaoFactory.getAdsDao().findByUserId(String.valueOf(user.getId())));
-        System.out.println(userId);
-        System.out.println(String.valueOf(userId));
-        System.out.println(user);
+
         request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String username = request.getParameter("updateUsername");
+        String email = request.getParameter("updateEmail");
+        User user = (User) request.getSession().getAttribute("user");
+        DaoFactory.getUsersDao().updateByUsername(username,email, user.getUsername());
+        User newUser = DaoFactory.getUsersDao().findByUsername(username);
+        request.getSession().setAttribute("user", newUser);
+
+        System.out.println(username);
+        System.out.println(email);
+        System.out.println(user.getUsername());
+//        request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+        response.sendRedirect("/profile");
     }
 }
